90942305b278046cc67ce6cd1ccdf07e63de686e
wesgarland <wesgarland>
2003-06-05 01:20:52 +0000

    Modified to use type definitions in ../slib instead of local re-definitions

--- squish/msgtrack.h
+++ squish/msgtrack.h
@@ -17,50 +17,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-#ifndef __TYPEDEFS_H_DEFINED
-#define __TYPEDEFS_H_DEFINED
-
-/* Handle the special defines for 32-bit flat model compilers */
-
-#if defined(__386__) || defined(__FLAT__)
-  typedef unsigned      bit;
-
-  typedef unsigned char byte;
-  typedef signed char   sbyte;
-
-  typedef unsigned short word;
-  typedef signed short   sword;
-
-  typedef unsigned int  dword;
-  typedef signed int    sdword;
-
-  typedef unsigned short ushort;
-  typedef   signed short sshort;
-
-  typedef unsigned long  ulong;
-  typedef   signed long  slong;
-#else
-  typedef unsigned      bit;
-
-  typedef unsigned char byte;
-  typedef signed char   sbyte;
-
-  typedef unsigned short word;
-  typedef signed short  sword;
-
-  typedef unsigned long dword;
-  typedef signed long   sdword;
-
-  typedef unsigned short ushort;
-  typedef   signed short sshort;
-
-  typedef unsigned long  ulong;
-  typedef   signed long  slong;
-#endif
-
-#endif
-
-
+#include "typedefs.h"
 
 /*--------------------------------------------------------------------------*/
 /* nodex.ndx                                                                */
