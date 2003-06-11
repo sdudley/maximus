9120a57d26efca5496a72057df74812d4e9fd96e
wesgarland <wesgarland>
2003-06-11 14:49:29 +0000

    Now extern "C" so that btree can find stricmp(), etc

--- unix/include/winstr.h
+++ unix/include/winstr.h
@@ -1,9 +1,19 @@
 #ifndef _WES_WINSTR_H
 #define _WES_WINSTR_H
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
 inline int stricmp(const char *a, const char *b);
 inline int strnicmp(const char *a, const char *b, size_t n);
 char *strupr(char *s);
 char *strlwr(char *s);
 char *itoa(int value, char *buffer, int radix);
 int memicmp(const void *p, const void *q, size_t length);
+
+#ifdef  __cplusplus
+}
+#endif
+
 #endif
