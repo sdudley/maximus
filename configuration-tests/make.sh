ce2bcd05e4de348fe6bf4e215dfec117e3ac77b7
wesgarland <wesgarland>
2003-06-11 23:30:50 +0000

    Modified for compatibility with GNU Make 3.80 and RedHat 5.2

--- configuration-tests/make.sh
+++ configuration-tests/make.sh
@@ -10,18 +10,44 @@
 #
 # 3.76 does NOT support this feature. 3.79 does.
 #
-[ ! "${MAKE}" ] && echo "\007Warning: configure did not set \${MAKE}!" >&2
+[ ! "${MAKE}" ] && echo "Warning: configure did not set \${MAKE}!" >&2
 [ ! "${MAKE}" ] && MAKE=make
+
+nonlecho()
+{
+  if [ -f "/usr/bin/printf" ] || [ -f "/bin/printf" ]; then
+    if [ "$2" ]; then
+      printf '%s' "$*"
+    else
+      printf "$1"
+    fi
+  else
+    if [ -f "/usr/ucb/echo" ]; then
+      /usr/ucb/echo -n "$*"
+    else
+      echo -n "$*"
+    fi
+  fi
+
+  return 0 # true
+}
+
+beep()
+{
+  nonlecho "\007"
+  return 0 # true
+}
+
 echo "Testing your version of GNU make: ${MAKE}"
 
-set -- `${MAKE} --version | grep GNU | sed -e "s/.*version[ ]*//" -e "s/,.*//" -e "s/\./ /g"`
+set -- `${MAKE} --version | grep GNU | sed -e "s/GNU Make//" -e "s/.*version[ ]*//" -e "s/,.*//" -e "s/\./ /g"`
 
 # failure: 
 #	yes - no good
 #	no - just fine
 #	else - probably just fine
 
-[ ! "$1" ] && failure=probably && echo "\007>>>>> This does not appear to be GNU make." >&2
+[ ! "$1" ] && failure=probably && beep && echo ">>>>> This does not appear to be GNU make." >&2
 [ "$1" -lt "3" ] && failure=yes
 [ "$1" -gt "3" ] && failure="probably"
 [ "$1" -eq "3" ] && [ "$2" -le "76" ] && failure=yes
@@ -31,6 +57,7 @@ set -- `${MAKE} --version | grep GNU | sed -e "s/.*version[ ]*//" -e "s/,.*//" -
 
 case "${failure}" in
 	yes)
+		beep
 		echo
 		echo " + You are using a version of GNU make (version `echo $*|sed 's/ /./g'`) which is"
 		echo "   known to be incompatible with the Maximus Makefiles. While it"
@@ -44,14 +71,14 @@ case "${failure}" in
 		retval=0
 		;;
 	*)
+		beep
 		echo
 		echo " + You are using a unrecognized version (`echo $*|sed 's/ /./g'`) of GNU make."
 		echo "   The development platforms run GNU make 3.79 and 3.80. Our"
 		echo "   guess is that your version will ${failure} work."
 		retval=0
 		echo
-		[ -f /usr/ucb/echo ] && /usr/ucb/echo -n "Press Enter to Continue: "
-		[ ! -f /usr/ucb/echo ] && echo -n "Press Enter to Continue: "
+		nonlecho "Press Enter to Continue: "
 		;;
 esac
 
